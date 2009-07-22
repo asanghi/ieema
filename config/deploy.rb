1d14fdd5b0020bec1700158cd94cdc1fbff90bc0
asanghi <asanghi@6b816444-0ca8-466d-9e21-97960377599f>
2009-07-22 08:29:12 +0000

    adding deploy info
    git-svn-id: svn+ssh://svn.risingsuntech.net/home/asanghi/svn/ieema@4 6b816444-0ca8-466d-9e21-97960377599f

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,32 +1,138 @@
 set :application, "ieema"
-set :repository,  "http://svn.risingsuntech.net/ieema"
+set :repo_name, "ieema"
+set :stage, "production"
+set :deploy_to, "/u/#{application}"
+set :user, "arunagw"
+set :repository, "http://svn.risingsuntech.net/#{repo_name}/"
+set :deploy_via, :remote_cache
+set :rails_env, 'production'
 
-# If you have previously been relying upon the code to start, stop 
-# and restart your mongrel application, or if you rely on the database
-# migration code, please uncomment the lines you require below
+set :domain, "188.40.34.142"
+set :domains, ["ieema.risingsunapps.com"]
+role :app, domain, :primary => true
+role :web, domain
+role :db, domain, :primary => true
 
-# If you are deploying a rails app you probably need these:
+namespace :deploy do
+  task :start, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
+  end
 
-# load 'ext/rails-database-migrations.rb'
-# load 'ext/rails-shared-directories.rb'
+  task :stop, :roles => :app do
+    # Do nothing.
+  end
 
-# There are also new utility libaries shipped with the core these 
-# include the following, please see individual files for more
-# documentation, or run `cap -vT` with the following lines commented
-# out to see what they make available.
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
+  end
+end
 
-# load 'ext/spinner.rb'              # Designed for use with script/spin
-# load 'ext/passenger-mod-rails.rb'  # Restart task for use with mod_rails
-# load 'ext/web-disable-enable.rb'   # Gives you web:disable and web:enable
+desc "After updating code we need to populate a new database.yml"
+task :after_update_code, :roles => :app do
+  require "yaml"
+  set :production_database_password, proc { Capistrano::CLI.password_prompt("Production database remote Password : ") }
+  database_file = YAML::load_file('config/database.yml.tmp')
+  # get rid of uneeded configurations
+  database_file.delete('test')
+  database_file.delete('development')
 
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
+  # Populate production element
+  database_file['production']['adapter'] = "mysql"
+  database_file['production']['database'] = "#{application}_production"
+  database_file['production']['username'] = "root"
+  database_file['production']['password'] = "root"
+  database_file['production']['socket'] = "/var/run/mysqld/mysqld.sock"
+  database_file['production']['encoding'] = "utf8"
+  put YAML::dump(database_file), "#{release_path}/config/database.yml", :mode => 0664
+end
 
-# If you aren't using Subversion to manage your source code, specify
-# your SCM below:
-# set :scm, :subversion
-# see a full list by running "gem contents capistrano | grep 'scm/'"
+after "deploy:setup", "nginx:virtual_host:create"
+#after "deploy:restart"# ,"nginx:virtual_host:enable"#, "nginx:reload"
 
-role :web, ""
+set :nginx_script_name, "#{application}_host_conf"
+set :nginx_file_path, "/opt/nginx/sites-available/#{nginx_script_name}"
+
+namespace :nginx do
+  desc "Reload nginx settings"
+  task :reload, :roles => :web do
+    run "sudo /etc/init.d/nginx reload"
+  end
+
+  desc "Restart nginx"
+  task :restart, :roles => :web do
+    run "sudo /etc/init.d/nginx restart"
+  end
+
+
+  namespace :virtual_host do
+    desc "Create a new virtual host"
+    task :create, :roles => :app, :only => {:primary => true} do
+      require 'erb'
+      upload_path = "#{shared_path}/nginx"
+      template = File.read("config/templates/nginx_virtual_host.erb")
+      file = ERB.new(template).result(binding)
+      put file, upload_path, :mode => 0644
+      run <<-EOF
+        sudo cp #{upload_path} #{nginx_file_path}
+      EOF
+    end
+
+    desc "Enable a virtual host"
+    task :enable, :roles => :web do
+      sudo "ln -fs /opt/nginx/sites-available/#{nginx_script_name} /opt/nginx/sites-enabled/#{nginx_script_name}"
+    end
+
+    desc "Destroy a virtual host"
+    task :destroy, :roles => :web do
+      nginx.virtual_host.disable
+      run "sudo rm /opt/nginx/sites-available/#{nginx_script_name}"
+    end
+
+    desc "Disable a virtual host"
+    task :disable, :roles => :web do
+      sudo "rm /opt/nginx/sites-enabled/#{nginx_script_name}"
+    end
+  end
+end
+
+
+
+desc "Install the production_log_analyzer"
+task :install_syslog_gems, :roles => :app do
+  sudo 'gem install production_log_analyzer'
+  sudo 'gem install rails_analyzer_tools'
+end
+
+#desc "Installs the syslog filter"
+#task :install_syslog_filter, :roles => :app do
+#  sudo %{echo "!#{application} *.* #{shared_path}/log/#{rails_env}.log" >> /etc/syslog.conf}
+#  sudo "/etc/init.d/sysklogd restart" # works for Ubuntu
+#end
+
+desc "Install Log Rotate Script"
+task :install_log_rotate_script, :roles => :app do
+  rotate_script = %Q{#{shared_path}/log/#{rails_env}.log {
+  daily
+  rotate 28
+  notifempty
+  missingok
+  compress
+  size 5M
+  copytruncate
+}}
+  put rotate_script, "#{shared_path}/logrotate_script"
+  sudo "cp #{shared_path}/logrotate_script /etc/logrotate.d/#{application}"
+  sudo "rm #{shared_path}/logrotate_script"
+end
+
+set :log_email_recipient, "arun.agrawal@risingsuntech.net"
+
+desc "Analyze production log and email results"
+task :analyze_logs, :roles => :app do
+  sudo %Q{chmod a+r #{shared_path}/log/*.gz}
+  run %Q{for file in #{shared_path}/log/*.gz; \
+         do gzip -dc "$file" | \
+         pl_analyze /dev/stdin -e #{log_email_recipient}; \
+         done}
+end
