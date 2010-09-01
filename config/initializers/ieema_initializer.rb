d33c209cea21e9f4f0c4ec27c72baf7b457573fc
Aditya Sanghi <asanghi@me.com>
2010-09-02 01:03:31 +0530

    fixed rails 3 issue pending

--- config/initializers/ieema_initializer.rb
+++ config/initializers/ieema_initializer.rb
@@ -1,7 +1,7 @@
-#ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS.merge!(:month_and_year => "%B %Y")
-#ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS.merge!(:month_and_year => "%B %Y")
-#ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS.merge!(:short_month_and_year => "%b %Y")
-#ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS.merge!(:short_month_and_year => "%b %Y")
+Date::DATE_FORMATS.merge!(:month_and_year => "%B %Y")
+Time::DATE_FORMATS.merge!(:month_and_year => "%B %Y")
+Date::DATE_FORMATS.merge!(:short_month_and_year => "%b %Y")
+Time::DATE_FORMATS.merge!(:short_month_and_year => "%b %Y")
 DEFAULT_BLANK_COMPONENTS = 3
 ADMIN_USERNAME = "admin"
 ADMIN_PASSWORD = "ieemaadmin"
